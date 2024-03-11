531413302e7fbf22f67b8fb877784c6b2744c31b
Irina Bubela <Iryna.Bubela@epam.com>
2024-03-11 21:56:13 +0100

    Module 2 - PR fixes

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -6,9 +6,8 @@ describe('Counter Component', () => {
   it('Test that component renders initial value provided in props(1)', () => {
     const initialValue = 5; 
     const renderResult = render(<Counter initialValue={initialValue} />);
-    const countElement = renderResult.getByText(`Count: ${initialValue}`);
     
-    expect(countElement).toBeInTheDocument();
+    expect(renderResult.getByText(`Count: ${initialValue}`)).toBeInTheDocument();
   });
 
   it('Test that a click event on "decrement" button decrements the displayed value(2)', () => {
@@ -25,7 +24,7 @@ describe('Counter Component', () => {
 
   it('Test that a click event on "increment" button increments the displayed value(3)', () => {
     const initialValue = 3; 
-    const renderResult =  render(React.createElement(Counter, { initialValue }));
+    const renderResult = render(<Counter initialValue={initialValue} />);
     
     const incrementButton = renderResult.getByText('Increment');
     const textElement = renderResult.getByTestId('count-display');
